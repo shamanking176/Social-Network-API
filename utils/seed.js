194840f1d18b768195a9d2cee51e9edcd15fc9ad
Slug <adamloppe6@gmail.com>
2023-06-13 15:28:38 -0400

    fixing get one route

---
+++ utils/seed.js
@@ -0,0 +1,50 @@
+const connection = require('../config/connection');
+const { Thought, User } = require('../models');
+const { getRandomName, getRandomAssignments } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing courses
+  await Thought.deleteMany({});
+
+  // Drop existing students
+  await User.deleteMany({});
+
+  // Create empty array to hold the students
+  const users = [];
+
+  // Loop 20 times -- add students to the students array
+  for (let i = 0; i < 20; i++) {
+    // Get some random assignment objects using a helper function that we imported from ./data
+    const thought = getRandomAssignments(20);
+
+    const username = getRandomName();
+    const email = `${username}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    const friend = getRandomName();
+
+    users.push({
+      username,
+      email,
+      thought,
+      friend,
+    });
+  }
+
+  // Add students to the collection and await the results
+  await User.collection.insertMany(users);
+
+  // Add courses to the collection and await the results
+  await Thought.collection.insertOne({
+    username: 'UCLA',
+    
+    users: [...users],
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
