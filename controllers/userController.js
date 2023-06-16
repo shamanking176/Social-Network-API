fab40d315959bfb125db659e09f2a2f760512d0a
Adam <adamloppe6@gmail.com>
2023-06-16 13:46:14 -0400

    fixed base routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,8 +21,7 @@ module.exports = {
           !user
             ? res.status(404).json({ message: 'No user with that ID' })
             : res.json({
-                user,
-                grade: await grade(req.params.userId),
+                user            
               })
         )
         .catch((err) => {
@@ -97,14 +96,14 @@ module.exports = {
     },
     updateUser(req, res) {
       User.findOneAndUpdate(
-      { _id: req.params.thoughtId },
+      { _id: req.params.userId },
       { $set: req.body },
       { runValidators: true, new: true }
     )
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: 'No thought with this id!' })
-          : res.json(thought)
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
